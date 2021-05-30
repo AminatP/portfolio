b08a4e2022fb02180fafb9bd86ac29adfb3944f6
Aminat Puebla <ap2296@njit.edu>
2021-05-30 14:10:32 -0400

    created contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Layout from "../components/Layout"
 import HomePage from "../components/HomePage"
 import Skills from "../components/Skills"
 import Projects from "../components/Projects"
+import Contact from "../components/Contact"
 export default () => {
   return <Layout>
-    <HomePage/>
-    <Skills/>
-    <Projects/>
+    <HomePage />
+    <Skills />
+    <Projects />
+    <Contact />
   </Layout>
 }
 // ...GatsbyImageSharpFluid
