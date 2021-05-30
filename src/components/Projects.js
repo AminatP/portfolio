850976a5742ea32960be0f0b44411a6397da970a
Aminat Puebla <ap2296@njit.edu>
2021-05-29 21:52:10 -0400

    added links to github

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,50 @@
 import React from "react"
 import Title from "./Title"
 import Image from "gatsby-image"
-import { FaGithub, FaHome } from "react-icons/fa"
+import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
+import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 import projects from "../constants/projects"
 
+const query = graphql`
+  {
+    file(relativePath: {eq: "projects-1.jpg"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
 const Projects = () => {
+  const {file:{childImageSharp:{fluid}}} = useStaticQuery(query);
   return <section className="section projects">
     <Title title="Projects"/>
     <div className="section-center projects-center">
       {projects.map((project)=>{
-        const {id, title, description} = project
+        const {id, title, description, stack, github, url} = project
 
-        return <article key={id}>
-          <h4>{title}</h4>
-          <p>{description}</p>
+        return <article key={id} className="project">
+          <Image fluid={fluid} className="project-img"/>
+          <div className="project-info">
+            <h3>{title}</h3>
+            <p className="project-desc">{description}</p>
+            <div className="project-stack">
+              {stack.map((item) => {
+                return <span key={item.id}>{item.title}</span>
+              })}
+            </div>
+            <div className="project-links">
+              <a href={github}>
+                <FaGithubSquare className="project-icon"/>
+              </a>
+              <a href={url}>
+                <FaShareSquare className="project-icon"/>
+              </a>
+            </div>
+          </div>
         </article>
       })}
     </div>
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -4,16 +4,34 @@ export default [
     id: 1,
     title: "CHAIN - NOON",
     description: "Gatsby gives frontend teams the tools and technology they need to build world-class web experiences. From marketing sites, to eCommerce stores, to documentation. Gatsby can help you build it.",
+    stack: [
+      {id: 1, "title": "html"},
+      {id: 2, "title": "css"},
+      {id: 3, "title": "javascript"}],
+    github: "https://github.com/AminatP/chai-noon",
+    url: "https://chai-noon.herokuapp.com",
 
   },
   {
     id: 2,
     title: "RADIUS",
     description: "Gatsby gives frontend teams the tools and technology they need to build world-class web experiences. From marketing sites, to eCommerce stores, to documentation. Gatsby can help you build it.",
+    stack: [
+      {id: 1, "title": "html"},
+      {id: 2, "title": "css"},
+      {id: 3, "title": "javascript"}],
+    github: "https://github.com/AminatP/radius",
+    url: "https://chai-noon.herokuapp.com",
   },
   {
     id: 3,
     title: "BIBLIOTECA",
     description: "Gatsby gives frontend teams the tools and technology they need to build world-class web experiences. From marketing sites, to eCommerce stores, to documentation. Gatsby can help you build it.",
+    stack: [
+      {id: 1, "title": "html"},
+      {id: 2, "title": "css"},
+      {id: 3, "title": "javascript"}],
+    github: "https://github.com/AminatP/Biblioteca",
+    url: "https://chai-noon.herokuapp.com",
   },
 ]
