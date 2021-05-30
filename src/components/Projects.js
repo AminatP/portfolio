9ab3a7da6e58bd0396c6acbcba67c503e3e6a632
Aminat Puebla <ap2296@njit.edu>
2021-05-29 21:07:16 -0400

    working on projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,23 @@
 import React from "react"
 import Title from "./Title"
-import Project from "./Project"
+import Image from "gatsby-image"
+import { FaGithub, FaHome } from "react-icons/fa"
 import { Link } from "gatsby"
+import projects from "../constants/projects"
+
 const Projects = () => {
-  return <section>
+  return <section className="section projects">
     <Title title="Projects"/>
+    <div className="section-center projects-center">
+      {projects.map((project)=>{
+        const {id, title, description} = project
+
+        return <article key={id}>
+          <h4>{title}</h4>
+          <p>{description}</p>
+        </article>
+      })}
+    </div>
   </section>
 }
 
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -2,20 +2,18 @@ import React from "react"
 export default [
   {
     id: 1,
-    title: "REACT",
-    desciption: "yada yada yada",
+    title: "CHAIN - NOON",
+    description: "Gatsby gives frontend teams the tools and technology they need to build world-class web experiences. From marketing sites, to eCommerce stores, to documentation. Gatsby can help you build it.",
 
   },
   {
     id: 2,
-    title: "JAVASCRIPT",
-    desciption: "yada yada yada",
-
+    title: "RADIUS",
+    description: "Gatsby gives frontend teams the tools and technology they need to build world-class web experiences. From marketing sites, to eCommerce stores, to documentation. Gatsby can help you build it.",
   },
   {
     id: 3,
-    title: "HTML",
-    desciption: "yada yada yada",
-
+    title: "BIBLIOTECA",
+    description: "Gatsby gives frontend teams the tools and technology they need to build world-class web experiences. From marketing sites, to eCommerce stores, to documentation. Gatsby can help you build it.",
   },
 ]
