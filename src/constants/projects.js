435631617cba526c8862a20f245b82cb0e637c9f
Aminat Puebla <ap2296@njit.edu>
2021-06-01 20:50:05 -0400

    projects order change

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -3,7 +3,7 @@ import React from "react"
 export default [
   {
     id: 1,
-    title: "CHAIN - NOON",
+    title: "CHAIN NOON",
     description: "Chain Noon is mock, fully functional e-commerce website that sells tea products. It was developed by myself and 3 other developers within 7 day period. Users can add products to the card that persists, update or delete from the card. It also has enhanced security features and it is deployed to Heroku via Travis CI for continuous integration. I tackled vertical slices of the site including pagination.",
     stack: [
       {id: 1, "title": "JS"},
@@ -18,20 +18,6 @@ export default [
   },
   {
     id: 2,
-    title: "RADIUS",
-    description: "Radius is a web application that aims to help users that are looking to move to the new city not only find an apartment but also get acquainted with the neighborhood before moving in. Application was built within 2.5 weeks by a team of four developers. I was solely responsible for Firebase Authentication and setting up Firestore for user data collection.",
-    stack: [
-      {id: 1, "title": "JS"},
-      {id: 2, "title": "React"},
-      {id: 3, "title": "Redux"},
-      {id: 4, "title": "Firebase"},
-      {id: 5, "title": "Google Maps API"}],
-    github: "https://github.com/AminatP/radius",
-    url: "https://radius-ny.web.app",
-    idx: 1,
-  },
-  {
-    id: 3,
     title: "BIBLIOTECA",
     description: "Biblioteca is a mock, auction platform for rare and antiquarian books. Payment is processed through ethereum based decentralized platform for convinience and extra security. To create this aplication I taught myself basics of the Solidity language so that I could write smart contracts for the application.",
     stack: [
@@ -43,6 +29,20 @@ export default [
       {id: 6, "title": "MetaMask"}],
     github: "https://github.com/AminatP/Biblioteca",
     url: "https://github.com/AminatP/Biblioteca",
+    idx: 1,
+  },
+  {
+    id: 3,
+    title: "RADIUS",
+    description: "Radius is a web application that aims to help users that are looking to move to the new city not only find an apartment but also get acquainted with the neighborhood before moving in. Application was built within 2.5 weeks by a team of four developers. I was solely responsible for Firebase Authentication and setting up Firestore for user data collection.",
+    stack: [
+      {id: 1, "title": "JS"},
+      {id: 2, "title": "React"},
+      {id: 3, "title": "Redux"},
+      {id: 4, "title": "Firebase"},
+      {id: 5, "title": "Google Maps API"}],
+    github: "https://github.com/AminatP/radius",
+    url: "https://radius-ny.web.app",
     idx: 2,
   },
 ]
